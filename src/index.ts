7b77312baa64137c3daaaf9cc1c4fa9bb617af54
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-10-09 18:33:15 +0100

    Add sorting functionality and main function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import * as R from 'ramda';
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat'; // import plugin
+
+import getSunsetSunrise from './api';
 import { Coordinates } from './types';
 
-// MAIN MODULE
+dayjs().format();
+dayjs.extend(customParseFormat);
 /**
  * Generates a random floating point number within the specified range
  * @param {number} min - Lower bound (inclusive) of the range
@@ -38,3 +43,40 @@ const randomLocations = R.ifElse(
   },
   R.compose(R.map(randomCoordinates), R.range(0))
 );
+
+/**
+ * Sorting helper function for sorting time strings
+ *
+ * Interprets time strings in the "h:mm:ss A" format, i.e. 0-12 hours (no padding), 00-59 minutes
+ * (padded to length 2), 00-59 seconds (padded to length 2), separated by colons. Followed by
+ * AM/PM designation in capital letters, separated by a space from the time.
+ *
+ * @param {string} a - First time to compare
+ * @param {string} b - Second time to compare
+ * @returns {number} Second difference between time A and time B
+ */
+const timeSorter = (a: string, b: string): number =>
+  dayjs(a, 'h:mm:ss A').unix() - dayjs(b, 'h:mm:ss A').unix();
+
+/**
+ * The main function.
+ *
+ * Queries the Sunrise and Sunset Times API for a given number of random locations. These locations
+ * are then sorted by the time of their sunrise and the length of the day for the day with the
+ * earliest sunrise is printed to the console.
+ *
+ * @param {number} count - The count of locations to request
+ */
+const main = async (count: number) => {
+  const response = await getSunsetSunrise(randomLocations(count));
+  const results = R.map(R.prop('results'), response);
+
+  const sortBySunrise: Array<any> = [...results].sort(
+    (location1: any, location2: any) =>
+      timeSorter(location1.sunrise, location2.sunrise)
+  );
+
+  console.log(sortBySunrise[0].day_length);
+};
+
+main(100);
