b0a347e2b88804b0d83242f8ef8a5b63c8ec19c9
Jakub-L <47220449+Jakub-L@users.noreply.github.com>
2021-10-09 15:41:52 +0100

    Add functionality for generating random coordinates

--- index.ts
+++ index.ts
@@ -1,3 +1,11 @@
+// JSDOC TYPE DEFINITIONS
+/**
+ * @typedef {Object} Coordinates
+ * @property {number} lat - Latitude represented as float between -90 (90 °S) and 90 (90 °N)
+ * @property {number} lng - Longitude represented as float between -180 (180 °W) and 180 (180 °E)
+ */
+
+// MAIN SECTION
 /**
  * Generates a random floating point number within the specified range
  * @param {number} min - Lower bound (inclusive) of the range
@@ -12,3 +20,10 @@ const randomFloat = (min: number, max: number): number => {
   return Math.random() * (max - min) + min;
 };
 
+/**
+ * Generates random latitude-longitude pair
+ * @returns {Coordinates} Random coordinates
+ */
+const randomCoordinates = (): { lat: number; lng: number } => {
+  return { lat: randomFloat(-90, 90), lng: randomFloat(-180, 180) };
+};
